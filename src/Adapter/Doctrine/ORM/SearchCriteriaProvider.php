burroaktoolinc/datatables-bundle

commit 6efccf53c185c20574e81995a265f3656e0d8d84

Fixes and changes for column filters (#369)
* Remove operator option on AbstractFilter because it is nowhere used

* AbstractFilter: get rid of $this->$key

This line is risky and inflexible as it implicitely requires each option
to have a property on the child class.

* For searchColumns, check isSearchable() in getter instead of before set

This fixes a bug which is demonstrated in the test cases included in
this commit.

This is not the most elegant solution, let me know if you have something
better.

* Make column search behaviour the same as for global search

This is the default behaviour that I would expect for a column search.

src/Adapter/Doctrine/ORM/SearchCriteriaProvider.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 
 namespace Omines\DataTablesBundle\Adapter\Doctrine\ORM;
 
+use Doctrine\ORM\Query\Expr;
 use Doctrine\ORM\Query\Expr\Comparison;
 use Doctrine\ORM\QueryBuilder;
 use Omines\DataTablesBundle\Column\AbstractColumn;
@@ -43,8 +44,7 @@ class SearchCriteriaProvider implements QueryBuilderProcessorInterface
                         continue;
                     }
                 }
-                $search = $queryBuilder->expr()->literal($search);
-                $queryBuilder->andWhere(new Comparison($column->getField(), $column->getOperator(), $search));
+                $queryBuilder->andWhere($this->getSearchComparison($column, $search));
             }
         }
     }
@@ -56,11 +56,19 @@ class SearchCriteriaProvider implements QueryBuilderProcessorInterface
             $comparisons = $expr->orX();
             foreach ($state->getDataTable()->getColumns() as $column) {
                 if ($column->isGlobalSearchable() && !empty($column->getField()) && $column->isValidForSearch($globalSearch)) {
-                    $comparisons->add(new Comparison($column->getLeftExpr(), $column->getOperator(),
-                        $expr->literal($column->getRightExpr($globalSearch))));
+                    $comparisons->add($this->getSearchComparison($column, $globalSearch));
                 }
             }
             $queryBuilder->andWhere($comparisons);
         }
+    }
+
+    private function getSearchComparison(AbstractColumn $column, string $search): Comparison
+    {
+        return new Comparison(
+            $column->getLeftExpr(),
+            $column->getOperator(),
+            (new Expr())->literal($column->getRightExpr($search)),
+        );
     }
 }

src/DataTableState.php
@@ -104,7 +104,10 @@ final class DataTableState
             $column = $this->dataTable->getColumn((int) $key);
             $value = $this->isInitial ? $search : $search['search']['value'] ?? '';
 
-            if ($column->isSearchable() && ('' !== mb_trim($value))) {
+            // We do not check for $column->isSearchable() here, because at this point the
+            // field option may not have been set yet. This makes the check for isSearchable()
+            // unreliable.
+            if ('' !== mb_trim($value)) {
                 $this->setColumnSearch($column, $value);
             }
         }
@@ -204,11 +207,13 @@ final class DataTableState
     /**
      * Returns an array of column-level searches.
      *
+     * @param bool $onlySearchable if true, only returns columns for which isSearchable() is true
      * @return SearchColumn[]
      */
-    public function getSearchColumns(): array
+    public function getSearchColumns(bool $onlySearchable = true): array
     {
-        return $this->searchColumns;
+        // `searchColumns` may include columns that are not searchable, so we filter them out here.
+        return array_filter($this->searchColumns, fn ($searchInfo) => !$onlySearchable || $searchInfo['column']->isSearchable());
     }
 
     public function setColumnSearch(AbstractColumn $column, string $search, bool $isRegex = false): static

src/Filter/AbstractFilter.php
@@ -16,29 +16,34 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 abstract class AbstractFilter
 {
-    protected string $template_html;
-    protected string $template_js;
-    protected string $operator;
+    /**
+     * @var array<string, mixed>
+     */
+    protected array $options = [];
+
+    public function __construct()
+    {
+        // Initialize the options with the default values set on the OptionsResolver
+        $this->set([]);
+    }
 
     /**
      * @param array<string, mixed> $options
      */
-    public function set(array $options): void
+    public function set(array $options): static
     {
         $resolver = new OptionsResolver();
         $this->configureOptions($resolver);
+        $this->options = $resolver->resolve($options);
 
-        foreach ($resolver->resolve($options) as $key => $value) {
-            $this->$key = $value;
-        }
+        return $this;
     }
 
     protected function configureOptions(OptionsResolver $resolver): static
     {
-        $resolver->setDefaults([
-            'template_html' => null,
-            'template_js' => null,
-            'operator' => 'CONTAINS',
+        $resolver->setRequired([
+            'template_html',
+            'template_js',
         ]);
 
         return $this;
@@ -46,17 +51,12 @@ abstract class AbstractFilter
 
     public function getTemplateHtml(): string
     {
-        return $this->template_html;
+        return $this->options['template_html'];
     }
 
     public function getTemplateJs(): string
     {
-        return $this->template_js;
-    }
-
-    public function getOperator(): string
-    {
-        return $this->operator;
+        return $this->options['template_js'];
     }
 
     abstract public function isValidValue(mixed $value): bool;

src/Filter/ChoiceFilter.php
@@ -16,11 +16,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ChoiceFilter extends AbstractFilter
 {
-    protected ?string $placeholder = null;
-
-    /** @var array<string, string> */
-    protected array $choices = [];
-
     protected function configureOptions(OptionsResolver $resolver): static
     {
         parent::configureOptions($resolver);
@@ -40,7 +35,7 @@ class ChoiceFilter extends AbstractFilter
 
     public function getPlaceholder(): ?string
     {
-        return $this->placeholder;
+        return $this->options['placeholder'];
     }
 
     /**
@@ -48,11 +43,11 @@ class ChoiceFilter extends AbstractFilter
      */
     public function getChoices(): array
     {
-        return $this->choices;
+        return $this->options['choices'];
     }
 
     public function isValidValue(mixed $value): bool
     {
-        return array_key_exists($value, $this->choices);
+        return array_key_exists($value, $this->getChoices());
     }
 }

src/Filter/TextFilter.php
@@ -16,8 +16,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class TextFilter extends AbstractFilter
 {
-    protected ?string $placeholder = null;
-
     protected function configureOptions(OptionsResolver $resolver): static
     {
         parent::configureOptions($resolver);
@@ -35,7 +33,7 @@ class TextFilter extends AbstractFilter
 
     public function getPlaceholder(): ?string
     {
-        return $this->placeholder;
+        return $this->options['placeholder'];
     }
 
     public function isValidValue(mixed $value): bool

tests/Functional/FunctionalTest.php
@@ -102,6 +102,17 @@ class FunctionalTest extends WebTestCase
         $this->assertCount(2, $json->data);
         $this->assertStringStartsWith('Company ', $json->data[0]->company);
         $this->assertSame('LastName24 (Company 4)', $json->data[0]->fullName);
+
+        // A column search should be based on substring matching by default (same as global
+        // search). Not on exact matching.
+        $json = $this->callDataTableUrl('/service?_dt=persons&draw=2&order[0][column]=2&order[0][dir]=desc&columns[1][search][value]=name24');
+        $this->assertCount(1, $json->data);
+        $this->assertSame('LastName24 (Company 4)', $json->data[0]->fullName);
+
+        // Search for `LastName1` in the first name column should return no results. This
+        // tests that the column search only applies to the specified column.
+        $json = $this->callDataTableUrl('/service?_dt=persons&draw=2&order[0][column]=2&order[0][dir]=desc&columns[1][search][value]=LastName1');
+        $this->assertCount(0, $json->data);
     }
 
     public function testCustomDataTable(): void

tests/Unit/Adapter/ORMAdapterTest.php
@@ -19,7 +19,9 @@ use Omines\DataTablesBundle\Adapter\Doctrine\ORMAdapter;
 use Omines\DataTablesBundle\DataTableFactory;
 use Omines\DataTablesBundle\DataTableState;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpFoundation\Request;
 use Tests\Fixtures\AppBundle\DataTable\Type\GroupedTableType;
+use Tests\Fixtures\AppBundle\DataTable\Type\ServicePersonTableType;
 
 class ORMAdapterTest extends KernelTestCase
 {
@@ -42,6 +44,32 @@ class ORMAdapterTest extends KernelTestCase
         $adapter = $datatable->getAdapter();
         $data = $adapter->getData(new DataTableState($datatable));
         iterator_to_array($data->getData());  // Evaluate the iterator to trigger the exception
+    }
+
+    /**
+     * Tests that column searches are applied when `field` is set by ORMAdapter.
+     *
+     * When `field` and `searchable` are not set manually, isSearchable() will only
+     * return true after the `field` option is set by ORMAdapter. This tests that the
+     * column search still is applied correctly.
+     */
+    public function testColumnSearch(): void
+    {
+        $datatable = $this->factory->createFromType(ServicePersonTableType::class)
+            ->setMethod(Request::METHOD_GET);
+        $datatable->handleRequest(Request::create('/?_dt=' . $datatable->getName() . '&columns[1][search][value]=John'));
+
+        // At this point, $searchColumns is empty because isSearchable() returns false due
+        // to `field` not being set yet. Ideally it should contain the search value.
+        $searchColumns = $datatable->getState()->getSearchColumns();
+        $this->assertCount(0, $searchColumns);
+
+        // After calling getData(), the column search should be correctly returned.
+        $datatable->getAdapter()->getData($datatable->getState());
+
+        $searchColumns = $datatable->getState()->getSearchColumns();
+        $this->assertCount(1, $searchColumns);
+        $this->assertSame('John', $searchColumns['firstName']['search']);
     }
 
     public function testORMAdapterQueryEvent(): void

tests/Unit/DataTableTest.php
@@ -98,7 +98,7 @@ class DataTableTest extends TestCase
         $this->assertSame(10, $state->getLength());
         $this->assertSame('foo', $state->getGlobalSearch());
         $this->assertCount(2, $state->getOrderBy());
-        $this->assertSame('bar', $state->getSearchColumns()['foo']['search']);
+        $this->assertSame('bar', $state->getSearchColumns(onlySearchable: false)['foo']['search']);
 
         // Test boundaries
         $state->setStart(-1);
@@ -109,6 +109,28 @@ class DataTableTest extends TestCase
 
         $column = $datatable->getColumn(0);
         $this->assertSame($state, $column->getState());
+    }
+
+    /**
+     * Tests that getSearchColumns only returns columns for which `isSearchable()` is true.
+     */
+    public function testDataTableStateSearchColumns(): void
+    {
+        $datatable = $this
+            ->createMockDataTable()
+            ->add('foo', TextColumn::class, ['searchable' => true])
+            ->add('bar', TextColumn::class, ['searchable' => false])
+            ->setMethod(Request::METHOD_GET)
+        ;
+        $datatable->handleRequest(Request::create('/?_dt=' . $datatable->getName()));
+
+        $state = $datatable->getState();
+        $state->setColumnSearch($datatable->getColumn(0), 'foo');
+        $state->setColumnSearch($datatable->getColumn(1), 'bar');
+
+        $searchColumns = $state->getSearchColumns();
+        $this->assertCount(1, $searchColumns);
+        $this->assertSame('foo', $searchColumns['foo']['search']);
     }
 
     public function testPostMethod(): void

commit d3dda5a8ef0a6c6430fbb5347637dc0ac07b9fa9

Update SearchCriteriaProvider.php
Fixes the upstream breakage in https://github.com/omines/datatables-bundle/pull/369  which removed ALL the filtering support for managing the query being generated

This adds filter support for left/right/operator BACK into the system

Ugh

src/Adapter/Doctrine/ORM/SearchCriteriaProvider.php
@@ -16,6 +16,7 @@ use Doctrine\ORM\Query\Expr;
 use Doctrine\ORM\Query\Expr\Comparison;
 use Doctrine\ORM\QueryBuilder;
 use Omines\DataTablesBundle\Column\AbstractColumn;
+use Omines\DataTablesBundle\Filter\AbstractFilter;
 use Omines\DataTablesBundle\DataTableState;
 
 /**
@@ -39,12 +40,15 @@ class SearchCriteriaProvider implements QueryBuilderProcessorInterface
             $search = $searchInfo['search'];
 
             if ('' !== mb_trim($search)) {
+                /* If we are using a filter we need to use the filter's left/right/operator */
                 if (null !== ($filter = $column->getFilter())) {
                     if (!$filter->isValidValue($search)) {
                         continue;
                     }
+                    $queryBuilder->andWhere($this->getFilterSearchComparison($filter, $column, $search));
+                } else {
+                    $queryBuilder->andWhere($this->getColumnSearchComparison($column, $search));
                 }
-                $queryBuilder->andWhere($this->getSearchComparison($column, $search));
             }
         }
     }
@@ -56,19 +60,28 @@ class SearchCriteriaProvider implements QueryBuilderProcessorInterface
             $comparisons = $expr->orX();
             foreach ($state->getDataTable()->getColumns() as $column) {
                 if ($column->isGlobalSearchable() && !empty($column->getField()) && $column->isValidForSearch($globalSearch)) {
-                    $comparisons->add($this->getSearchComparison($column, $globalSearch));
+                    $comparisons->add($this->getColumnSearchComparison($column, $globalSearch));
                 }
             }
             $queryBuilder->andWhere($comparisons);
         }
     }
 
-    private function getSearchComparison(AbstractColumn $column, string $search): Comparison
+    private function getColumnSearchComparison(AbstractColumn $column, string $search): Comparison
     {
         return new Comparison(
             $column->getLeftExpr($column->getField()),
             $column->getOperator(),
             (new Expr())->literal($column->getRightExpr($search)),
+        );
+    }
+
+    private function getFilterSearchComparison(AbstractFilter $filter, AbstractColumn $column, string $search): Comparison
+    {
+        return new Comparison(
+            $filter->getLeftExpr($column->getField()),
+            $filter->getOperator(),
+            (new Expr())->literal($filter->getRightExpr($search)),
         );
     }
 }